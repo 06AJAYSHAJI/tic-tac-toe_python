7aaa33802cef54cc4c993c3420f7955b9a6710c6
Replit user <>
2022-10-12 19:35:05 +0000

    computermove and select random is done

--- tictactoe.py
+++ tictactoe.py
@@ -1,64 +1,102 @@
-board = ['' for i in range(10)]
+import random
+
+board = [' ' for i in range(10)]
 
 
-def insertletter(letter='', pos=0):
+def insertLetter(letter, pos):
   board[pos] = letter
 
 
-def printboard(board):
-  print('  |  |  ')
-  print("" + board[1] + "  | " + board[2] + " | " + board[3] + "  ")
-  print('  |  |  ')
-  print('_________')
-  print('  |  |  ')
-  print("" + board[4] + "  |  " + board[5] +"|" + board[6] + "  ")
-  print('  |  |  ')
-  print('_________')
-  print('  |  |  ')
-  print("" + board[7] + "  | " + board[8] + "| " + board[9] + "  ")
-  print('  |  |  ')
-  print('_________')
+def printBoard(board):
+  print("   |   |   ")
+  print(" " + board[1] + " | " + board[2] + " | " + board[3] + " ")
+  print("   |   |   ")
+  print(" ---------")
+  print("   |   |   ")
+  print(" " + board[4] + " | " + board[5] + " | " + board[6] + " ")
+  print("   |   |   ")
+  print(" ---------")
+  print("   |   |   ")
+  print(" " + board[7] + " | " + board[8] + " | " + board[9] + " ")
+  print("   |   |   ")
 
 
-def isfreespace(pos):
-  return board[pos] == ''
+def isFreeSpace(pos):
+  return board[pos] == ' '
 
 
-def isboardfull(board):
-  if board.cound('') > 1:
+def isBoardFull(board):
+  if board.count(' ') > 1:
     return False
   else:
     return True
 
 
-def iswinner(b, l):
-  return (b[1] == l and b[2] == l and b[3] == l
-          or b[4] == l and b[5] == l and b[6] == l
-          or b[7] == l and b[8] == l and b[9] == l
-          or b[1] == l and b[4] == l and b[7] == l
-          or b[2] == l and b[5] == l and b[8] == l
-          or b[3] == l and b[6] == l and b[9] == l
-          or b[1] == l and b[5] == l and b[9] == l
-          or b[3] == l and b[5] == l and b[7] == l)
+def isWinner(board, letter):
+  return (board[1] == letter and board[2] == letter and board[3] == letter
+          or board[4] == letter and board[5] == letter and board[6] == letter
+          or board[7] == letter and board[8] == letter and board[9] == letter
+          or board[1] == letter and board[4] == letter and board[7] == letter
+          or board[2] == letter and board[5] == letter and board[8] == letter
+          or board[3] == letter and board[6] == letter and board[9] == letter
+          or board[1] == letter and board[5] == letter and board[9] == letter
+          or board[3] == letter and board[5] == letter and board[7] == letter)
 
 
-def playermove():
+def playerMove():
   run = True
   while run:
-    move = input('enter the possition of from 1 to 9')
+    move = input("Enter the position of X between 1 to 9")
     try:
       move = int(move)
       if move > 0 and move < 10:
-        if isfreespace(move):
+        if isFreeSpace(move):
           run = False
-          insertletter('x', move)
-          printboard(board)
+          insertLetter("X", move)
+          printBoard(board)
         else:
-          print('the possition is alredy exist')
+          print("The position is already occupied")
       else:
-        print('enter the number between 1 to 9')
+        print("Enter a number between 1-9")
     except:
-      print('enter the number')
+      print("Enter an integer")
 
 
-playermove()
+def computermove():
+  possibleMove = [
+    x for x, letter in enumerate(board) if letter == ' ' and x != 0
+  ]
+  move = 0
+
+  for let in ['0', 'x']:
+    for i in possibleMove:
+      boardcopy = board[:]
+      boardcopy[i] = let
+      if isWinner(boardcopy, let):
+        move = i
+        return move
+  cornerMove = []
+  for i in possibleMove:
+    if i in [1, 3, 7, 9]:
+      cornerMove.append()
+  if len(cornerMove) > 0:
+    move = selectRandom(cornerMove)
+    return move
+  if 5 in possibleMove:
+    move = 5
+    return move
+
+  edgeOpen = []
+
+  for i in possibleMove:
+    if i in [2, 4, 6, 8]:
+      edgeOpen.append()
+
+  if len(edgeOpen) > 0:
+    move = selectRandom(edgeOpen)
+    return move
+
+  def selectRandom(li):
+    ln = len(li)
+    r = random.randRange(0, ln)
+    return li[r]
